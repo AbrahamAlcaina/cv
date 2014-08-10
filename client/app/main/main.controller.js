7c782d71fb485fe607fd8accbea43cfbd5b921c6
abraham.alcaina@gmail.com <abraham.alcaina@gmail.com>
2014-08-10 19:33:31 +0200

    ~ skills and tweet

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,7 +6,16 @@ angular.module('cvApp')
             $translate.use(langKey);
         };
         $scope.$translate = $translate;
+
         $timeout(function() {
         	$scope.showName = true;
-        }, 1000);
+        }, 500);
+
+        $scope.skills = [
+        	{title:'WEB_DESIGN', percentage:'70'}, 
+        	{title:'WEB_DEVELOPMENT', percentage:'80'},
+        	{title:'SOFTWARE_ENGINERING', percentage:'95'},
+        	{title:'BACKED_DEVELOPMENT', percentage:'85'},
+        	{title:'TEAM_MANAGEMENT', percentage:'90'}
+        ];
     });
