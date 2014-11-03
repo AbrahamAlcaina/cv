1a8302a7fc2372c8079cce4c2aa4c5468893a737
Abraham Alcaina <abraham.alcaina@gmail.com>
2014-11-03 18:53:41 +0100

    full screen header.

---
+++ client/app/main/box.jquery.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    fullScreenContainer();    
+    setTimeout(function() {
+        fullScreenContainer();
+    }, 100);
+
+
+
+});
+
+function fullScreenContainer() {
+
+    var screenWidth = $(window).width() + "px";
+
+    if ($(window).height() > 500) {
+        var screenHeight = $(window).height() + "px";
+    } else {
+        var screenHeight = "500px";
+    }
+
+
+    $("#home").css({
+        width: screenWidth,
+        height: screenHeight
+    });
+}
